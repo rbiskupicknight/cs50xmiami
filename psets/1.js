47b6e5562eb2fe7b4b520bc5b6cc8d354b358de8
unknown <rbiskupic-knight@RBISKUPICKN-2W7.tfanet.org>
2015-09-03 20:21:48 -0400

    Completed

--- psets/1.js
+++ psets/1.js
@@ -10,3 +10,10 @@ Forgot how old someone is? Calculate it!
 */
 
 // write your solution here...
+
+var current_year = 2015;
+var birth_year = 1988;
+var age_old= current_year - birth_year;
+var age_young = age_old - 1;
+
+console.log("They are either " + age_old + " or " + age_young + " ")
