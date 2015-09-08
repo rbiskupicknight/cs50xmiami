17872f63cf4b305949ea2c6196c7649ba00ad0d2
Rob Biskupic-Knight <biskupic.knight@gmail.com>
2015-09-07 20:53:24 -0400

    Completed

--- psets/6.js
+++ psets/6.js
@@ -12,6 +12,15 @@ For example:
 Look up the JavaScript string reference to find methods which may be useful!
 */
 
+var first_word = process.argv[2];
+var second_word = process.argv[3];
+var combination = mixUp(first_word, second_word);
+console.log(combination);
+
 function mixUp(a, b) {
-  // write your solution here...
+  var temp_aa = a[0];
+  var temp_ab = a[1];
+  var new_a = a.replace(a[0], b[0]).replace(a[1], b[1]);
+  var new_b = b.replace(b[0], temp_aa).replace(b[1], temp_ab);
+  return(new_a + " " + new_b);
 }
