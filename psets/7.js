d5abbab404414e7d84833d92b93cad0f2a3314ab
Rob Biskupic-Knight <biskupic.knight@gmail.com>
2015-09-07 21:11:39 -0400

    Completed

--- psets/7.js
+++ psets/7.js
@@ -10,7 +10,13 @@ For example:
 
 fixStart('babble'): 'ba**le'
 */
+var word = process.argv[2];
+var message = fixStart(word)
+console.log(message)
 
 function fixStart(s) {
-  // write your solution here...
+  var first_letter = s[0]
+  var first = new RegExp(first_letter,"g");
+  var new_string = s.replace(first, "*").replace("*",first_letter)
+  return(new_string)
 }
